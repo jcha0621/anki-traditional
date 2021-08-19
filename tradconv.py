2fe55424e55b760efd27f7abfc6809ab9cdc3c51
Peter Wu <peterlingwu@gmail.com>
2021-08-18 20:26:57 -0700

    Convert all note fields to traditional

--- tradconv.py
+++ tradconv.py
@@ -1,4 +1,5 @@
 import sys, os
+from hanziconv import HanziConv
 
 # Load Anki library
 sys.path.append('anki') 
@@ -9,9 +10,25 @@ cpath = os.path.join(os.getcwd(), 'collection.anki2')
 # Load the collection
 col = Collection(cpath)
 
-# Use the available methods to list the notes
-# need to extend to all tags (maybe use col.tags?)
-for cid in col.findNotes('tag:A1'): 
-    note = col.getNote(cid)
-    front = note.fields[0]
-    print(front)
+# fields = ['Word', 'Word Alternatives', 'Word Hidden Alternatives', 
+#     'Word Typing Corrects', 'Definition', 'Definition Alternatives', 
+#     'Definition Hidden Alternatives', 'Definition Typing Corrects', 
+#     'Pronunciation', 'Pronunciation Alternatives', 
+#     'Pronunciation Hidden Alternatives', 'Pronunciation Typing Corrects', 
+#     'Part of Speech', 'Gender', 'Audio', 'Definition -> Word Mem', 'Level', 
+#     'Thing']
+
+# print(col.decks.all_names_and_ids())
+for deck_metadata in col.decks.all_names_and_ids():
+    for tag in col.tags.byDeck(deck_metadata.id):
+        for cid in col.find_notes(f'tag:{tag}'): 
+            note = col.getNote(cid)
+            for i in range(8): # Only update up to Definition Typing Corrects
+                note.fields[i] = HanziConv.toTraditional(note.fields[i])
+            col.update_note(note)
+
+# Save changes to collection
+
+col.save()
+col.close(downgrade=True)
+print('all done!')
